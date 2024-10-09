7d71b50d0749cb1aac023b594e722427742a36a9
franzsuperales <franz.daniel_superalse@questronix.com.ph>
2024-10-09 12:15:43 +0800

    edited urls.py

--- posapp/urls.py
+++ posapp/urls.py
@@ -6,10 +6,10 @@ from django.views.decorators.csrf import csrf_exempt
 app_name = 'posapp'
 urlpatterns = [
     path('',views.posapp, name='posapp'),
-    path('list',views.itemList, name='item_list'),
-    path('add-menu',views.itemAdd, name='item_add'),
+    path('itemList',views.itemList, name='item_list'),
+    path('itemAdd',views.itemAdd, name='item_add'),
     path('update-items/', views.itemUpdate, name="item_update"),
-    path('delete-menu/<str:pk>', views.itemDelete, name="item_delete"),
+    path('itemDelete/<str:pk>', views.itemDelete, name="item_delete"),
     path('confirm_order', views.confirm_order, name='confirm_order'),
     path('sales-dashboard/', views.TotalSalesDashboardView.as_view(),
          name='sales_dashboard'),
